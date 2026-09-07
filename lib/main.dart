7794b374c4531da62e8c97a0b6ed900774a70b1d
Isaac <reame025@d.umn.edu>
2026-09-06 22:37:08 -0500

    No clue at what happened, I had it working perfectly fine with being able to see it on the emulator but something happened along the way and everything is messed up.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'home_screen.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class HomeworkTrackerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Homework Tracker',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -25,34 +27,61 @@ class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late Timer _timer;
+
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3), () {
+
+    _timer = Timer(
+      const Duration(seconds: 3), () {
+        if (!mounted) return;
+
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen()),
       );
     });
   }
 
   @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const FourRotatingDotsLoadingScreen(); 
+  }
+}
+
+class FourRotatingDotsLoadingScreen extends StatelessWidget {
+  const FourRotatingDotsLoadingScreen({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-            'Homework Tracker', 
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LoadingAnimationWidget.fourRotatingDots(
+              color: Colors.blue,
+              size: 100.0,
             ),
+          const SizedBox(height: 20),
+          const Text(
+            'Loading... Loading...',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
